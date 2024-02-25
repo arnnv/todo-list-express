5c5638e790a7bd2301da7fba2f0a0ea22e736882
Arnav Gupta <109935951+arnnv@users.noreply.github.com>
2024-02-25 14:39:48 +0530

    Add GET handler for /todos

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
+app.get("/todos", (req, res) => {
+  fs.readFile("./todos.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}.`);
 });
