b884120c343ed7d5a88b3242ec19350bc0eae89c
Arnav Gupta <109935951+arnnv@users.noreply.github.com>
2024-02-25 14:56:49 +0530

    Change ID type from integer to string

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
 const app = express();
@@ -11,6 +12,10 @@ function getTodoIdx(arr, id) {
   return -1;
 }
 
+function generateRandomId() {
+  return uuidv4();
+}
+
 app.get("/todos", (req, res) => {
   fs.readFile("./todos.json", "utf-8", (err, data) => {
     if (err) throw err;
@@ -21,7 +26,7 @@ app.get("/todos", (req, res) => {
 app.get("/todos/:id", (req, res) => {
   fs.readFile("./todos.json", "utf-8", (err, data) => {
     if (err) throw err;
-    const id = parseInt(req.params.id);
+    const id = req.params.id;
     let arr = JSON.parse(data);
 
     let todoIdx = getTodoIdx(arr, id);
@@ -32,6 +37,8 @@ app.get("/todos/:id", (req, res) => {
     }
   });
 });
+
+app.post("/todos", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}.`);
