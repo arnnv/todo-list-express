05358415f0d489ed0988fe6cbba1d6035324dc34
Arnav Gupta <109935951+arnnv@users.noreply.github.com>
2024-02-25 14:51:14 +0530

    Add GET handler for /todos/:id

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
+function getTodoIdx(arr, id) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) return i;
+  }
+  return -1;
+}
+
 app.get("/todos", (req, res) => {
   fs.readFile("./todos.json", "utf-8", (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
+  });
+});
+
+app.get("/todos/:id", (req, res) => {
+  fs.readFile("./todos.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    const id = parseInt(req.params.id);
+    let arr = JSON.parse(data);
+
+    let todoIdx = getTodoIdx(arr, id);
+    if (todoIdx === -1) {
+      res.sendStatus(404);
+    } else {
+      res.json(arr[todoIdx]);
+    }
   });
 });
 
